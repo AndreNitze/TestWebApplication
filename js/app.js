d7db55828b804d1a76b2748c4d3e55e3acdf34bc
AndreNitze <andre.nitze87@gmail.com>
2014-04-11 12:35:40 +0200

    Add some functionality

--- js/app.js
+++ js/app.js
@@ -1,7 +1,28 @@
 angular.module('tutorialApp', [])
-    .controller('PersonsCtrl', function($scope, $http){
+    .factory('History', function() {
+        var items = [];
+        return {
+            getItems: function() {
+                return items;
+            },
+            addDrink: function(drink) {
+                items.push(drink);
+            }
+        };
+    })
+    .controller('HistoryCtrl', function($scope, $http, History){
 
+       // $scope.history = History;
+
+        $http.get('https://mobile-quality-research.org/services/drinks/').then(function(historyResponse) {
+            $scope.drinks = historyResponse.data;
+        });
+
+       // $scope.drinks = History;
+
+        /*
         $http.get('https://www.mobile-quality-research.org/services/meals').success(function(data) {
             $scope.meals = data;
         });
+        */
     });
